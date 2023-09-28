da017382681aec216ab3b723f7d707f990ad8f6f
gopiKhareedu421 <khareedugopi2000@gmail.com>
2023-09-27 23:27:58 -0400

    Implemented INotifyPropertyChanged and added properties to track the 'IsBusy' status, 'Title' for page titles, and 'IsNotBusy' as its inverse in the BaseViewModel, enabling better communication between the view and view model.

---
+++ 3/dotnet-maui-workshop-main/Part 2 - MVVM/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -0,0 +1,48 @@
+﻿namespace MonkeyFinder.ViewModel;
+
+public class BaseViewModel : INotifyPropertyChanged
+{
+    bool isBusy;
+    //public bool IsBusy
+    //{
+    //    get => isBusy;
+    //    set
+    //    {
+    //        if (isBusy == value)
+    //            return;
+    //        isBusy = value;
+    //        OnPropertyChanged();
+    //    }
+    //}
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+                return;
+            isBusy = value;
+            OnPropertyChanged();
+            // Also raise the IsNotBusy property changed
+            OnPropertyChanged(nameof(IsNotBusy));
+        }
+    }
+    public bool IsNotBusy => !IsBusy;
+
+    string title;
+    public string Title
+    {
+        get => title;
+        set
+        {
+            if (title == value)
+                return;
+            title = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
